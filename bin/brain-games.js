43fc03f8d73995e4a1267204eca847c9cd27d2b0
Vitaly Melnikov <vmelnikov@bk.ru>
2020-03-17 22:48:12 +0300

    Adding readline-sync library

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+import requestName from '../src/index.js';
+
 console.log('Welcome to the Brain Games!');
+
+// const name = readlineSync.question('May I have your name?');
+// console.log(`Hello, ${name}!`);
+
+requestName();
