b2d0e3e6e8171e95cfd0674fa088d65bcacd16bf
Vitaly Melnikov <vmelnikov@bk.ru>
2020-03-19 22:09:32 +0300

    Adding github action & badge

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,7 +4,4 @@ import requestName from '../src/index.js';
 
 console.log('Welcome to the Brain Games!');
 
-// const name = readlineSync.question('May I have your name?');
-// console.log(`Hello, ${name}!`);
-
 requestName();
